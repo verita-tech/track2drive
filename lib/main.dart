8776660fa51bd7b2478bdc0de511c78dc6a9a567
Tim Rentrop <timrentrop@proton.me>
2025-12-03 16:11:05 +0100

    Adding firebase with stage dev and options for multistaging.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options_dev.dart' as dev;
 
-void main() {
+enum AppEnv { dev, staging, prod }
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final firebaseOptions = _getFirebaseOptions();
+  await Firebase.initializeApp(options: firebaseOptions);
+
   runApp(const MainApp());
 }
 
@@ -10,11 +20,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
+}
+
+FirebaseOptions _getFirebaseOptions() {
+  if (kDebugMode || kProfileMode) {
+    return dev.DefaultFirebaseOptions.currentPlatform;
+  }
+  throw UnimplementedError('No FirebaseOptions for this platform');
 }
