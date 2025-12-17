8a2f6c4085fd388b1305b099a24f8dfda2f9af54
Tim Rentrop <timrentrop@proton.me>
2025-12-17 13:25:25 +0100

    Removing dead code.

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -20,8 +20,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     final email = widget.user.email ?? 'Unbekannt';
